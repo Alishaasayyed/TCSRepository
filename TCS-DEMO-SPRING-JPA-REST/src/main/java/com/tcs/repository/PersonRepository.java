2c00290c28c9cd34631493b897e994ddcdff8aa0
Alisha <alishaasayyed786@gmail.com>
2022-03-29 22:57:24 -0700

    JPA project

---
+++ TCS-DEMO-SPRING-JPA-REST/src/main/java/com/tcs/repository/PersonRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.tcs.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tcs.entity.Person;
+
+import java.util.List;
+
+public interface PersonRepository<P> extends CrudRepository<Person, Long> {
+	// This PersonRepository inharite the CurdRepository in which we can implement
+	// the various method save() findOne() count() delete() deleteAll()
+	List<Person> findByFirstName(String firstName);
+}
